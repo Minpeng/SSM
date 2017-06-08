3be6586fc7ff49eb9bd09b4e3a6f689aa90558b4
pengmin <pengmin3@163.com>
2017-06-08 16:43:56 +0800

    shiro 首页

--- ssm/src/main/java/com/ssm/controller/testController.java
+++ ssm/src/main/java/com/ssm/controller/testController.java
@@ -1,9 +1,10 @@
 package com.ssm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by pengmin on 2017/5/6.
@@ -15,6 +16,7 @@ public class testController {
     @RequestMapping("/index")
     @ResponseBody
     public  String test(HttpServletRequest request){
-        return  "test";
+
+		return "test";
     }
 }
